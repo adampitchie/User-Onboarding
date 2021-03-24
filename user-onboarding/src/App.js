56d36fb0fc9a538cf67eaac518384c09eca2f241
Adam Pitchie <adampitchie@gmail.com>
2021-03-24 13:51:24 -0700

    working app

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import Form from './Form'
+import schema from './formSchema'
+import * as yup from 'yup'
+import axios from 'axios'
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  tos: false,
+}
+
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+}
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(true)
+
+  const postNewUser = (newUser) => {
+    axios
+    .post('https://reqres.in/api/users', newUser)
+    .then(res => {
+      setUsers(newUser)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  const inputChange = (name, value) => {
+    yup
+      .reach(schema, name) // get to this part of the schema
+      //we can then run validate using the value
+      .validate(value) // validate this value
+      .then(() => {
+        // happy path and clear the error
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      // if the validation is unsuccessful, we can set the error message to the message
+      // returned from yup (that we created in our schema)
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          // validation error from schema
+          [name]: err.errors[0],
+        });
+      });
+
+    setFormValues({
+      ...formValues, [name]: value
+    })
+  }
+
+  const formSubmit = () => {
+    console.log('firing')
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      tos: formValues.tos
+    }
+    // TODO: Create New User
+    postNewUser(newUser)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form 
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
     </div>
   );
 }
---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+export default function Form(props) {
+  const { values, submit, change, disabled, errors } = props
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  }
+
+  const onChange = (evt) => {
+    const { name, value, type, checked } = evt.target
+    const valueToUse = type === 'checkbox' ? checked : value;
+    change(name, valueToUse)
+  }
+
+  return(
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className='inputs'>
+          <label>
+            User
+            <input
+              value={values.name}
+              onChange={onChange}
+              name='name'
+              type='text'
+            />
+          </label>
+
+          <label>
+            Email
+            <input
+              value={values.email}
+              onChange={onChange}
+              name='email'
+              type='text'
+            />
+          </label>
+
+          <label>
+            Password
+            <input
+              value={values.password}
+              onChange={onChange}
+              name='password'
+              type='text'
+            />
+          </label>
+
+          <label>
+            Terms of Service
+            <input
+              value={values.tos}
+              onChange={onChange}
+              name='tos'
+              type='checkbox'
+            />
+          </label>
+
+          <button>Submit</button>
+        </div>
+
+        <div className='errors'>
+          <div>{errors.name}</div>
+          <div>{errors.email}</div>
+          <div>{errors.password}</div>
+        </div>
+      </form>
+    </div>
+  )
+
+}
